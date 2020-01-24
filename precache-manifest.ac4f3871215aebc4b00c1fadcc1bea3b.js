5f788768ae25b1d9671d30402abbc876a2337bbc
tomatillodesign <chris@tomatillodesign.com>
2020-01-24 09:24:15 -0500

    Updates

---
+++ precache-manifest.ac4f3871215aebc4b00c1fadcc1bea3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207c03ae9e1f57ae76660af4d03d94b5",
+    "url": "/bookshelf/index.html"
+  },
+  {
+    "revision": "563a743414f7e68cda43",
+    "url": "/bookshelf/static/css/main.0b29b694.chunk.css"
+  },
+  {
+    "revision": "f6c13a60e4580daaa470",
+    "url": "/bookshelf/static/js/2.a0f609b8.chunk.js"
+  },
+  {
+    "revision": "181f293ad1e96378d174507cf477a254",
+    "url": "/bookshelf/static/js/2.a0f609b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "563a743414f7e68cda43",
+    "url": "/bookshelf/static/js/main.a4f00a6f.chunk.js"
+  },
+  {
+    "revision": "3de14a845fa8adff260b",
+    "url": "/bookshelf/static/js/runtime-main.c1168acc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bookshelf/static/media/logo.5d5d9eef.svg"
+  }
+]);
