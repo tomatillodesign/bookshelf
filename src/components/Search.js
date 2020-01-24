bcc1eeea82044d361e12f56071337e66651914f2
Chris Liu-Beers <chris@tomatillodesign.com>
2020-01-24 15:05:50 -0500

    router 305 fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -56,7 +56,7 @@ render() {
 
           if (this.state.toResults) {
            return <Redirect to={{
-             pathname: "/#results",
+             pathname: "/results",
              data: { results: this.state.results }
            }} />
          }
