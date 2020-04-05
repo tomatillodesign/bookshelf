787987dbe25d507b21222a565bc312a71f35018b
tomatillodesign <chris@tomatillodesign.com>
2020-04-05 15:12:07 -0400

    datepicker cleanup

--- src/components/Label.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Label = (props) => (
-  <label htmlFor={props.name}>{props.children}</label>
-);
-
-export default Label;
--- src/components/LightBook.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBook } from '@fortawesome/pro-light-svg-icons';
-
-const LightBook = (props) => (
-     <FontAwesomeIcon icon={faBook} />
-)
-
-export default LightBook;
